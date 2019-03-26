9594a62b7e8b3f39e592754dfa3a88fd72864d5a
PiotrPwlk <piotr.pawlak101@gmail.com>
2019-03-26 23:37:13 +0100

    First deploy completed.

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,16 @@
 const knex = require('knex');
 
+const connection = {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'twierdza1',
+    database : 'BarberShop'
+}
 const db = knex({
     client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'twierdza1',
-        database: 'BarberShop'
-    }
+    debug: true,
+    connection: process.env.DATABASE_URL || connection,
+    ssl: true
   });
 
 export default db;
