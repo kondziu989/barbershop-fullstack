b35eac813cfe045d443c4c537bd01f5408387e09
Konrad <kondziu989@gmail.com>
2019-04-05 12:18:49 +0200

    Register on localhost in progress

--- frontend/src/components/registerComponent/registerDialog.tsx
+++ frontend/src/components/registerComponent/registerDialog.tsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, InputAdornment, IconButton, FormControl, SnackbarContent } from "@material-ui/core";
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, InputAdornment, IconButton, FormControl, SnackbarContent, LinearProgress } from "@material-ui/core";
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
+
+interface Error{
+  index:number,
+  message: string
+}
+
+interface NewUser{
+  email: string,
+  password: string,
+  firstname: string,
+  lastname: string,
+  phone: string
+}
+
+enum Status{
+  none,
+  pending,
+  accepted,
+  rejected
+}
+
+const query = (data: NewUser) => 
+`mutation {
+  register(UserInput:${data}) {
+    firstName
+    idU
+    email
+  }
+}
+`
 
 export default class RegisterDialog extends React.Component {
   readonly EMAIL = 1;
@@ -10,6 +40,7 @@ export default class RegisterDialog extends React.Component {
   readonly FIRST = 3;
   readonly LAST = 4;
   readonly PHONE = 5;
+  readonly EMPTY_MESSAGE = "To pole nie może być puste"
 
 
     state = {
@@ -19,65 +50,116 @@ export default class RegisterDialog extends React.Component {
       firstname:"",
       lastname:"",
       phone:"",
-      error_message: "",
-      showError:false,
-      errorCode:Array<number>()
+      errors:Array<Error>(),
+      responseStatus: null,
+      status: Status.none
     };
+
     
-  
+
+    
+    fetchNewUser = () =>{
+        fetch("https://mohawkbarbershop.herokuapp.com/graphql", {
+          method: "POST",
+          headers: {
+            //'Accept': 'application/json',
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            query
+          })
+        })
+          .then(res => res.json())
+          .then(data => {this.setState({ response:data.data.register.status})})
+    }
+
     handleClickOpen = () => {
       this.setState({ open: true });
     };
   
     handleClose = () => {
-      this.setState({ open: false });
+      this.setState({ 
+        open: false,
+        errors: Array<Error>()
+      });
     };
 
     handleAccept = () => {
-
-      if(this.validateInput()){
-      }
-      else
-
-
+      this.validateInput();
       console.log(this.state);
     }
     validateInput = () => {
-      this.clearErrorCode()
-      if(!this.validateEmail())
-        return false
-      if(!this.validatePassword())
-        return false
-      if(!this.validateRequired())
-        return false
+      const errors: Array<Error> = []
+      this.validateEmail(errors)
+      this.validatePassword(errors)
+      this.validateFirstName(errors)
+      this.validateLastName(errors)
+      this.validatePhone(errors)
+      this.setState({errors:errors})
+
+      if(errors.length===0){
+        //this.fetchNewUser()
+        this.setState({status: Status.pending})
+      }
     }
 
-    clearErrorCode = () => {
-      this.setState({errorCode:Array<number>()});
-    }
-
-    validateEmail = () => {
+    validateEmail = (errors:Array<Error>) => {
       let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
       if (regexp.test(String(this.state.email).toLowerCase())){
         return true
       }
-      this.setState({error_message: "E-mail w złym formacie!"})
-      this.setState({errorCode: this.state.errorCode.concat(this.EMAIL)})
-        return false
-    }
-
-    validatePassword = () => {
-      if(this.state.password.length > 5)
-      return true
-      
-      console.log("KROTKIE HASLO")
-      this.setState({error_message: "Hasło zbyt krótkie!"});
-      this.setState({errorCode: this.state.errorCode.concat(this.PASSWORD)})
+      errors.push({
+        index: this.EMAIL,
+        message: "E-mail w złym formacie"
+      })
       return false
     }
 
-    validateRequired = () => {
+    validatePassword = (errors:Array<Error>) => {
+      if(this.state.password.length > 5)
       return true
+      
+      errors.push({
+        index: this.PASSWORD,
+        message: "Hasło zbyt krótkie"
+      })
+      return false
+    }
+
+    validateFirstName = (errors:Array<Error>) => {
+      if(this.state.firstname.length===0){
+        errors.push({
+          index: this.FIRST,
+          message: this.EMPTY_MESSAGE
+        })
+        return false
+      }
+      return true
+    }
+    validateLastName = (errors:Array<Error>) => {
+      if(this.state.lastname.length===0){
+        errors.push({
+          index: this.LAST,
+          message: this.EMPTY_MESSAGE
+        })
+        return false
+      }
+      return true
+    }
+    validatePhone = (errors:Array<Error>) => {
+      if(this.state.phone.length===0){
+        errors.push({
+          index: this.PHONE,
+          message: this.EMPTY_MESSAGE
+        })
+        return false
+      }
+      return true
+    }
+
+    getErrorMessage = (index: number) =>{
+      let err = this.state.errors.filter((err)=> err.index===index);
+      return err.length===0?false:err[0].message
     }
     
     handleEmailChange = (event: any) =>{
@@ -104,6 +186,12 @@ export default class RegisterDialog extends React.Component {
       this.setState({phone: event.target.value})
       console.log(this.state)
     }
+
+    showLinearQuery = () => {
+
+      return this.state.status===Status.pending?<LinearProgress color="primary" variant="query"/>:null
+
+    }
   
     render() {
       return (
@@ -124,56 +212,59 @@ export default class RegisterDialog extends React.Component {
               <TextField
                 required
                 autoFocus
-                error={this.state.errorCode.includes(this.EMAIL)?true:false}
                 margin="dense"
                 id="email"
                 label="Email"
                 type="email"
                 fullWidth
                 onChange={this.handleEmailChange}
-                helperText={this.state.errorCode.includes(this.EMAIL)?this.state.error_message:false}
+                error={this.state.errors.map((err)=> err.index===this.EMAIL?true:false).includes(true)}
+                helperText={this.getErrorMessage(this.EMAIL)}
+                  
               />
               <TextField
                 required
-                error={this.state.errorCode.includes(this.PASSWORD)?true:false}
                 margin="dense"
                 id="password"
                 label="Hasło"
                 type="password"
                 fullWidth
                 onChange={this.handlePasswordChange}
-                helperText={this.state.errorCode.includes(this.PASSWORD)?this.state.error_message:false}
-
+                error={this.state.errors.map((err)=> err.index===this.PASSWORD?true:false).includes(true)}
+                helperText={this.getErrorMessage(this.PASSWORD)}
               />
               <TextField
                 required
-                error={this.state.errorCode.includes(this.FIRST)?true:false}
                 margin="dense"
-                id="name"
+                id="first-name"
                 label="Imię"
                 type="name"
                 fullWidth
                 onChange={this.handleFirstNameChange}
+                error={this.state.errors.map((err)=> err.index===this.FIRST?true:false).includes(true)}
+                helperText={this.getErrorMessage(this.FIRST)}
               />
               <TextField
                 required
-                error={this.state.errorCode.includes(this.LAST)?true:false}
                 margin="dense"
                 id="last-name"
                 label="Nazwisko"
                 type="last-name"
                 fullWidth
                 onChange={this.handleLastNameChange}
+                error={this.state.errors.map((err)=> err.index===this.LAST?true:false).includes(true)}
+                helperText={this.getErrorMessage(this.LAST)}
               />
               <TextField
                 required
-                error={this.state.errorCode.includes(this.PHONE)?true:false}
                 margin="dense"
                 id="phone"
                 label="Telefon"
                 type="phone"
                 fullWidth
                 onChange={this.handlePhoneChange}
+                error={this.state.errors.map((err)=> err.index===this.PHONE?true:false).includes(true)}
+                helperText={this.getErrorMessage(this.PHONE)}
               />
             </DialogContent>
             <DialogActions>
@@ -184,6 +275,7 @@ export default class RegisterDialog extends React.Component {
                 Zatwierdź
               </Button>
             </DialogActions>
+            {this.showLinearQuery()}
           </Dialog>
         </>
       );
